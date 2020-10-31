dce3929c1cdd20fb71b1f4d2d5bce09240e9c7ee
K. Medha Chaitanya <medha_chaitanya@hotmail.com>
2020-10-31 19:54:50 -0400

    implemented reporting piecharts

--- controller/MainPage.controller.js
+++ controller/MainPage.controller.js
@@ -160,9 +160,103 @@ sap.ui.define([
 			}
 			
 		},
+		resetEverythingPress : function(oEvent)
+		{
+			oStorage.clear()
+		},
+		graph: function(oEvent)
+		{
+			document.getElementById("my_dataviz").innerHTML = "";
+			this.byId("idSummaryHours").removeAllItems();
+			// set the dimensions and margins of the graph
+			var width =$(window).width();
+			var height = $(window).height()/2;
+			var margin = 50
+
+			// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+			var radius = Math.min(width, height) / 2 - margin
+
+			// append the svg object to the div called 'my_dataviz'
+			var svg = d3.select("#my_dataviz")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+			.append("g")
+			.attr('viewBox','0 0 ' + height + ' ' + width)
+			.attr('preserveAspectRatio', 'xMinYMin')
+			.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+			var modelData = oStorage.get("myLocalData")
+			var array = JSON.parse(modelData).Entries;
+			//group by these
+			var result = [];
+			array.reduce(function(res, value) {
+			if (!res[value.Category]) {
+				res[value.Category] = { Category: value.Category, TotalTimeWorkedInHours: 0 };
+				result.push(res[value.Category])
+			}
+			res[value.Category].TotalTimeWorkedInHours += parseFloat(value.TotalTimeWorkedInHours);
+			return res;
+			}, {});
+			//converting in to desired form
+			var data = result.reduce(function(acc, cur, i) {
+				acc[cur.Category] = cur;
+				return acc;
+			}, {});
+
+			for(var i=0; i<result.length;i++)
+			{
+				this.byId("idSummaryHours").addItem(new sap.m.DisplayListItem({label: result[i].Category , value:result[i].TotalTimeWorkedInHours +" H"}))
+			}
+
+
+			// set the color scale
+			var color = d3.scaleOrdinal()
+			.domain(data)
+			.range(d3.schemeDark2);
+
+			// Compute the position of each group on the pie:
+			var pie = d3.pie()
+			.value(function(d) {
+				return d.value.TotalTimeWorkedInHours;
+			})
+			var data_ready = pie(d3.entries(data))
+			// Now I know that group A goes from 0 degrees to x degrees and so on.
+
+			// shape helper to build arcs:
+			var arcGenerator = d3.arc()
+			.innerRadius(0)
+			.outerRadius(radius)
+
+			// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+			svg
+			.selectAll('mySlices')
+			.data(data_ready)
+			.enter()
+			.append('path')
+			.attr('d', arcGenerator)
+			.attr('fill', function(d){ return(color(d.data.key)) })
+			.attr("stroke", "black")
+			.style("stroke-width", "2px")
+			.style("opacity", 0.7)
+
+			// Now add the annotation. Use the centroid method to get the best coordinates
+			svg
+			.selectAll('mySlices')
+			.data(data_ready)
+			.enter()
+			.append('text')
+			.text(function(d){ return d.data.key})
+			.attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+			.style("text-anchor", "middle")
+			.style("font-size", 17)
+
+			
+			
+		},
 		deleteReportsPress : function(oEvent)
-		{			
-			oStorage.clear();			
+		{								
 			var emptyModel = { "Entries" : []};				
 			oStorage.put("myLocalData",JSON.stringify(emptyModel));			
 			MessageBox.success("Cleared all the report entries");
